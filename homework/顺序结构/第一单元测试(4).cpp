2f59b319c463f34709c0cf5b91f7685ef8fc3031
Minecraaf <86233879+Minecraaf@users.noreply.github.com>
2021-06-25 19:48:18 +0800

    Create 第一单元测试(4).cpp

---
+++ homework/顺序结构/第一单元测试(4).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{ 
+   char A;
+   cin>>A;
+    cout<<" "<<" "<<A<<" "<<" "<<endl;
+      cout<<' '<<A<<A<<A<<' '<<endl;
+        cout<<A<<A<<A<<A<<A<<endl ;
+      cout<<' '<<A<<A<<A<<' '<<endl;
+    cout<<" "<<" "<<A<<" "<<" "<<endl;
+    return 0;
+}
